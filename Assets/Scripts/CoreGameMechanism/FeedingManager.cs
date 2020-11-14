99b1b2093655917d69de09b496cc15f70c79179e
Neltharion59 <lukawh5959@gmail.com>
2020-11-14 23:40:53 +0100

    Available food panel
    - In feeding screen, there is grid with available food items
    - can be dragged, are put back when mouse released

--- Assets/Scripts/CoreGameMechanism/FeedingManager.cs
+++ Assets/Scripts/CoreGameMechanism/FeedingManager.cs
@@ -1,19 +1,110 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
 class FeedingManager : Singleton<FeedingManager>
 {
     public GameObject FeedingPanel;
+    public GameObject FoodInventoryPanel;
+
+    public GameObject IconUR;
+    public GameObject IconUL;
+    public GameObject IconLR;
+    public GameObject IconLL;
+
+    public GameObject InventoryItem;
+
+    public readonly int InventoryGridSizeRows = 4;
+    public readonly int InventoryGridSizeColumns = 10;
 
     private void Awake()
     {
-        FeedingPanel.SetActive(false);
+        //FeedingPanel.SetActive(false);
+    }
+    private void Start()
+    {
+        
+    }
+    private void FillGrid()
+    {
+        Debug.LogError("Filling grid");
+        // https://answers.unity.com/questions/989697/grid-layout-group-scalable-content.html
+        RectTransform parentRect = FoodInventoryPanel.GetComponent<RectTransform>();
+        GridLayoutGroup gridLayout = FoodInventoryPanel.GetComponent<GridLayoutGroup>();
+
+        if (parentRect == null) { Debug.LogError("Rect is null"); }
+        if (gridLayout == null) { Debug.LogError("Grid Layout is null"); }
+
+        gridLayout.cellSize = new Vector2(parentRect.rect.width / InventoryGridSizeColumns, parentRect.rect.height / InventoryGridSizeRows);
+        for (int i = 0; i < InventoryGridSizeRows; i++)
+        {
+            for (int j = 0; j < InventoryGridSizeColumns; j++)
+            {
+                GameObject InventoryItem = Instantiate(this.InventoryItem);
+                Item Apple = ItemManager.Instance.GetItem("Apple");
+                InventoryItem.GetComponent<Image>().sprite = Apple.icon;
+                
+                InventoryItem.transform.SetParent(FoodInventoryPanel.transform, false);
+                //InventoryItem.GetComponent<Draggable>().OriginalPosition = InventoryItem.transform.position;
+            }
+        }
     }
     public void InitiateDayEnd()
     {
-        FeedingPanel.SetActive(true);
+        //FeedingPanel.SetActive(true);
     }
     public void InitiateDayStart(string _)
     {
-        FeedingPanel.SetActive(false);
-        DayCycleManager.Instance.StartDay();
+        FillGrid();
+        /*FeedingPanel.SetActive(false);
+        DayCycleManager.Instance.StartDay();*/
+
+        /* List<GameObject> Icons = new List<GameObject>();
+         Icons.Add(IconUL);
+         Icons.Add(IconUR);
+         Icons.Add(IconLL);
+         Icons.Add(IconLR);*/
+
+        /* Vector3 IconPosition = IconUL.transform.position;
+         Debug.Log(IconPosition);
+
+          IconPosition = IconUR.transform.position;
+         Debug.Log(IconPosition);
+
+          IconPosition = IconLL.transform.position;
+         Debug.Log(IconPosition);
+
+         IconPosition = IconLR.transform.position;
+         Debug.Log(IconPosition);*/
+        /*foreach (GameObject GameObject in Icons)
+        {
+            Debug.Log(GameObject.transform.localPosition);
+        }
+        foreach (GameObject GameObject in Icons)
+        {
+            Debug.Log(GameObject.GetComponent<RectTransform>().anchoredPosition);
+        }
+        foreach (GameObject GameObject in Icons)
+        {
+            Debug.Log(GameObject.GetComponent<RectTransform>().sizeDelta);
+        }
+        Vector3[] Corners = new Vector3[] { new Vector3(), new Vector3(), new Vector3(), new Vector3()};
+        FeedingPanel.GetComponent<RectTransform>().GetWorldCorners(Corners);
+        for (int i = 0; i < Corners.Length; i++)
+        {
+            Debug.Log(Corners[i]);
+        }
+        int j = 0;
+        foreach (GameObject GameObject in Icons)
+        {
+            Corners = new Vector3[] { new Vector3(), new Vector3(), new Vector3(), new Vector3() };
+            GameObject.GetComponent<RectTransform>().GetWorldCorners(Corners);
+            Debug.Log("Icon number "+ j);
+            for (int i = 0; i < Corners.Length; i++)
+            {
+                Debug.Log(Corners[i]);
+            }
+            j++;
+        }*/
     }
 }
---
+++ Assets/Scripts/Util/Draggable.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Draggable : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+    RectTransform m_transform = null;
+    public Vector3 OriginalPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_transform = GetComponent<RectTransform>();
+        OriginalPosition = new Vector3();
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        m_transform.position += new Vector3(eventData.delta.x, eventData.delta.y);
+        OriginalPosition += new Vector3(eventData.delta.x, eventData.delta.y);
+        /* Debug.Log(OriginalPosition);
+         Debug.Log(m_transform.position);*/
+        // magic : add zone clamping if's here.
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.LogError("Pressed");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.LogError("Released");
+        m_transform.position -= OriginalPosition;
+    }
+}
