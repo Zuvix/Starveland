c9d3b1ded76e56af8cea035fc047cfdc9a24e791
zeliskamichal53 <zeliskamichal53@gmail.com>
2020-11-15 10:06:30 +0100

    combat preparation

---
+++ Assets/Scripts/Samo/ActivityStateWander.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ActivityStateWander : ActivityState
+{
+    private UnitCommandMove MoveCommand;
+    private UnitCommandIdle IdleCommand;
+
+    public ActivityStateWander() : base()
+    {
+        this.IdleCommand = new UnitCommandIdle();
+    }
+
+    public override void InitializeCommand(Unit Unit)
+    {
+        base.InitializeCommand(Unit);
+        Unit.SetCommand(this.IdleCommand);
+    }
+
+    public override IEnumerator PerformAction(Unit Unit)
+    {
+        return null;
+    }
+}
+
--- Assets/Scripts/Samo/UnitCommandIdle.cs
+++ Assets/Scripts/Samo/UnitCommandIdle.cs
@@ -7,15 +7,19 @@ using System.Threading.Tasks;
 
 public class UnitCommandIdle : UnitCommand
 {
+    private bool isIdling = false;
     public UnitCommandIdle() : base(null)
     {
     }
     public override bool IsDone(Unit Unit)
     {
-        return false;
+        //return false;
+        return isIdling;
     }
     public override IEnumerator PerformAction(Unit Unit)
     {
+        isIdling = true;
         yield return Unit.StartCoroutine(Unit.BeIdle());
+        isIdling = false;
     }
 }
