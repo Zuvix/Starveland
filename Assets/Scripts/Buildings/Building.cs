5168bf5e35fedcd5acbe58b1cf9cb4acae0afa04
Neltharion59 <lukawh5959@gmail.com>
2020-12-08 13:59:56 +0100

    Building visitor count in left panel

--- Assets/Scripts/Buildings/Building.cs
+++ Assets/Scripts/Buildings/Building.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections.Generic;
+using UnityEngine.Events;
 
 public abstract class Building : CellObject
 {
     public List<Resource> ConstructionCost;
     public List<Unit> CurrentVisitors = new List<Unit>();
-
+    public UnityEvent<Building> OnVisitorsChanged = new UnityEvent<Building>();
     public void Enter(Unit Unit)
     {
         this.CurrentVisitors.Add(Unit);
         Unit.EnterBuilding(this);
         Unit.ToggleVisibility(false);
+        OnVisitorsChanged.Invoke(this);
     }
     public void Leave(Unit Unit)
     {
@@ -21,10 +23,12 @@ public abstract class Building : CellObject
             ExitCell.Map.CenterObject(ExitCell.x, ExitCell.y, Unit.gameObject);
             Unit.ToggleVisibility(true);
         }
+        OnVisitorsChanged.Invoke(this);
     }
     public void LeaveDead(Unit Unit)
     {
         this.CurrentVisitors.Remove(Unit);
         Unit.LeaveBuildingDead();
+        OnVisitorsChanged.Invoke(this);
     }
 }
