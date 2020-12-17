89ab7cdcf6b837670224c3171b7696de2c793799
Neltharion59 <lukawh5959@gmail.com>
2020-12-17 14:17:12 +0100

    Ship progress panel - item cardinalities popup

---
+++ Assets/Scripts/UiVisuals/GUIReference.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIReference : Singleton<GUIReference>
+{
+    public GameObject ShipProgressPopupPanel;
+    public List<Image> ShipProgressMaterialIcons;
+    public List<TMP_Text> ShipProgressMaterialLabels;
+}
---
+++ Assets/Scripts/UiVisuals/ShipProgressPanel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShipProgressPanel : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        GUIReference.Instance.ShipProgressPopupPanel.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        GUIReference.Instance.ShipProgressPopupPanel.SetActive(false);
+    }
+}
