245b197964225b1d9987cb202f9f476753486d92
sweil9 <samuelkubala@gmail.com>
2020-12-20 02:32:08 +0100

    Small UI upgrades

---
+++ Assets/Prefabs/UI/FishItemUi.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FishItemUi : MonoBehaviour
+{
+    public TMP_Text percentTxt;
+    public Image icon;
+    public void SetStats(Sprite sprite,int chance)
+    {
+        icon.sprite = sprite;
+        percentTxt.text = chance + "%";
+    }
+}
